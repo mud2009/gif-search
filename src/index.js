acb3575d528a4e78233413d177d24bd30301215a
Matt Eilar <meilar@gmail.com>
2022-02-07 13:24:35 -0800

    initial commit
    Co-authored-by: Matt Eilar <meilar@gmail.com>
    Co-authored-by: Will Greenberg <mud2009@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import testCode from './test-code.js';
 
+let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=5&offset=0&rating=pg-13&lang=en`
+
 $(document).ready(function() {
   let msg = testCode();
   console.log("This page is loading correctly " + msg);
 });
+
