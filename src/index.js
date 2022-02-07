071bb3897bcb13053333608ba7b2031e1b337dcc
Matt Eilar <meilar@gmail.com>
2022-02-07 15:10:01 -0800

    add trending gifs button
    Co-authored-by: Matt Eilar <meilar@gmail.com>
    Co-authored-by: Will Greenberg <mud2009@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -56,3 +56,31 @@ $('#random-btn').on("click",function(event){
     $('#output').html(`<img class="result-img" src="${response.data.images.original.url}">`);
   }
 });
+
+$('#trending-btn').on("click",function(event){
+  event.preventDefault();
+
+  let request = new XMLHttpRequest();
+  let url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&offset=0&rating=pg-13&lang=en`;
+
+  request.onreadystatechange = function(){
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response){
+    let gifArr = []; 
+    for (let index = 0; index <= 4; index += 1) {
+      gifArr.push(response.data[index].images.original.url);
+    }
+    gifArr.forEach(function(gif) {
+      $('#output').append(`<img class="result-img" src="${gif}">`);
+    });
+  }
+
+})
