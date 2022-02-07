72aeac1d4849480f51bb976fdbc6448f302d19b7
Matt Eilar <meilar@gmail.com>
2022-02-07 14:28:48 -0800

    add basic gif search that returns five results
    Co-authored-by: Matt Eilar <meilar@gmail.com>
    Co-authored-by: Will Greenberg <mud2009@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,12 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import testCode from './test-code.js';
 
-let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=5&offset=0&rating=pg-13&lang=en`
 
-$(document).ready(function() {
-  let msg = testCode();
-  console.log("This page is loading correctly " + msg);
+
+$("#search-btn").on("click", function(event) {
+  event.preventDefault();
+  const query = $("#searchbox").val();
+
+  let request = new XMLHttpRequest();
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=5&offset=0&rating=pg-13&lang=en`;
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log("If statement is true");
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response){
+    let gifArr = []; 
+    for (let index = 0; index <= 4; index += 1) {
+      gifArr.push(response.data[index].images.original.url);
+    }
+    gifArr.forEach(function(gif) {
+      $('#output').append(`<img class="result-img" src="${gif}">`);
+    });
+  }
 });
-
