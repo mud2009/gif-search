234ae05a526021ad5f42e911d8b014d4d2225e76
Will Greenberg <greenberg.will@gmail.com>
2022-02-08 11:32:34 -0800

    add gifTrending static method

--- src/gif.js
+++ src/gif.js
@@ -3,15 +3,45 @@ export default class Gif {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&limit=5&offset=0&rating=pg-13&lang=en`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve (request.response);
-      } else {
-        reject (request.response);
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-    })
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve (request.response);
+        } else {
+          reject (request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  static gifSearch(query) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=5&offset=0&rating=pg-13&lang=en`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve (request.response);
+        } else {
+          reject(request.resonse);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  static gifTrending() {
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&offset=0&rating=pg-13&lang=en`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve (request.respone);
+        } else {
+          reject (request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Gif from './gif.js'
+import Gif from './gif.js';
 
 // $("#search-btn").on("click", function(event) {
 //   event.preventDefault();
@@ -59,6 +59,28 @@ $(document).ready(function(){
     let promise = Gif.getRandom();
     promise.then(function(response){
       const body = JSON.parse(response);
+      $('#output').html(`<img class="result-img" src="${body.data.images.original.url}">`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+  $("#search-btn").click(function(event){
+    event.preventDefault();
+    const query = $("#searchbox").val();
+    let promise = Gif.gifSearch(query);
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      $('#output').html(`<img class="result-img" src="${body.data[0].images.original.url}">`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+  $("#trending-btn").click(function(event){
+    event.preventDefault();
+    let promise = Gif.gifTrending();
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      console.log(body);
       $('#output').html(`<img class="result-img" src="${body.data.images.original.url}">`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
